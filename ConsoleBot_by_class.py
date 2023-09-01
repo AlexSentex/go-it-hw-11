c5138cb9d1350324b36837fcf097ed7b63201f90
Oleksandr <alexsentex7@gmail.com>
2023-09-01 22:17:51 +0300

    Update ConsoleBot_by_class.py

--- ConsoleBot_by_class.py
+++ ConsoleBot_by_class.py
@@ -1,7 +1,8 @@
 from collections import UserDict
 from collections.abc import Iterator
-import time
 from datetime import datetime
+from prettytable import PrettyTable
+from dataclasses import dataclass
 
 class AddressBook(UserDict):
     '''The only Address book'''
@@ -12,133 +13,130 @@ class AddressBook(UserDict):
         return cls.instance
 
     def __init__(self) -> None:
-        super(AddressBook, self).__init__()
+        super().__init__()
         self.data = {}
-        self.cache = []
-        self.idx_sheet = [0, 2]
+        self.idx = 0
+        self.end_idx = 0
 
     def add_record(self, record):
         '''Add some contact'''
         key = record.name.value
         self.data[key] = record
 
-    def show_all(self):
-        '''Show all contacts'''
-        self.cache = []
-        for username, rec in self.items():
-            days_to_birthday = rec.days_to_birthday()\
-                    if rec.days_to_birthday() else '------'
-
-            if rec.phones:
-                self.cache.append('|{:^15}|{:^15}|{:^16}|\n'.format(
-                            username.title(),
-                            rec.phones[0].value,
-                            days_to_birthday
-                            ))
-                if len(rec.phones) == 1:
-                    continue
-
-                for num in rec.phones[1:]:
-                    self.cache.append('|{:^15}|{:^15}|{:^16}|\n'.format(
-                        '', num.value, '------'
-                        ))
-            else:
-                self.cache.append('|{:^15}|{:^15}|{:^16}|\n'.format(
-                            username, ' ', days_to_birthday
-                            ))
-
-
     def __next__(self):
-        if self.idx_sheet[0] >= self.idx_sheet[1]:
-            self.idx_sheet = [0, 2]
+        sheet = PrettyTable()
+        keys = tuple(enumerate(self.data.keys()))
+        sheet.field_names = ['Name', 'Phone', 'Days to birthday']
+        if self.end_idx == len(self):
+            self.idx = 0
+            self.end_idx = 0
             raise StopIteration
-        sheet = '|{:^15}|{:^15}|{:^16}|\n'.format(
-                                'Username',
-                                'Phone',
-                                'Days to birthday'
-                                )
-        for string in self.cache[self.idx_sheet[0] : self.idx_sheet[1]]:
-            sheet += string
-
-        self.idx_sheet.pop(0)
-        if (self.idx_sheet[0] + 2) <= len(self.cache):
-            self.idx_sheet.append(self.idx_sheet[0] + 2)
-            
-        else: self.idx_sheet.append(len(self.cache))
+        self.end_idx = self.idx + 2 if (self.idx + 2) <= len(self) else len(self)
+        for _, key in keys[self.idx : self.end_idx]:
+            sheet.add_row([
+                key,
+                self.data[key].phone.value,
+                self[key].days_to_birthday()
+            ])
+            self.idx += 1
         return sheet
 
-class AddressBookIter:
-    '''Seting up the Iterator for AddressBook'''
     def __iter__(self) -> Iterator:
-        return ab
+        return self
 
-class Record:
-    def __init__(self, name, phone=None, birthday=None) -> None:
-        self.name = name
-        self.birthday = birthday
-        self.phones = []
-        if phone:
-            if phone not in self.phones:
-                self.phones.append(phone)
+    def handler(self, command: str, args) -> str:
+        '''Handle commands'''
 
-    def add(self, phone):
-        self.phones.append(phone)
-
-    def edit(self, phone_to_edit, new_phone):
-        pass
-
-    def days_to_birthday(self):
-        if self.birthday.value:
-            now = datetime.now()
-            birthday = self.birthday.value
-            next_birthday = birthday.replace(year=now.year)
-            if next_birthday < now:
-                next_birthday = next_birthday.replace(year=next_birthday.year + 1)
-            return next_birthday.toordinal() - now.toordinal()
+        errors = {
+            1: 'Enter valid command!',
+            2: 'Username not found',
+            3: 'Enter valid username and(or) phone (phone must contain only digits)',
+            4: 'Number not found',
+            5: 'Please enter date using format "YYYY-MM-DD"'
+        }
+        
+        def greet():
+            return 'Hello!\nHow can I help you?'
 
 
-class Field:
-    pass
-    # def __init__(self) -> None:
-    #     self.value = {}
-    # def __setattr__(self, __name: str, __value: Any) -> None:
-    #     self.value[__name] = __value
+        def add(name_value: str, phone_value=None, birth_value=None) -> str:
+            '''Add user number and/or birthday into database'''
+            try:
+                if birth_value:
+                    birth_value = datetime.strptime(birth_value, '%Y-%m-%d')
+            except ValueError as err:
+                raise DateError from err
+
+            if name_value in ab.keys():
+                if phone_value:
+                    phone = Phone()
+                    phone.value = phone_value
+                    ab[name_value].add(phone)
+                if birth_value:
+                    birthday = Birthday()
+                    birthday.value = birth_value
+                    ab[name_value].birthday = birthday
+                return 'Done'
+
+            name = Name()
+            name.value = name_value
+
+            phone = Phone()
+            phone.value = phone_value
+
+            birthday = Birthday()
+            birthday.value = birth_value
+            
+            rec = Record(name, phone, birthday)
+            ab.add_record(rec)
+            return 'Done'
 
 
-class Name(Field):
-    def __init__(self, name) -> None:
-        self.value = name
+        def change(name, old_number, new_number) -> str:
+            '''Change user number'''
 
+            if name not in self.keys():
+                raise UsernameError
 
-class Birthday(Field):
-    def __init__(self, birthday) -> None:
-        self.value = birthday
+            for phone in self[name].phones:
+                if phone.value == old_number:
+                    phone.value = new_number
+                    return 'Done'
 
-class Phone(Field):
-    def __init__(self, phone) -> None:
-        self.value = phone
+            raise ValueError
 
+        def show(name: str) -> str:
+            '''Show User phone.\n
+            If key: 'all' - show all contacts'''
 
-class UsernameError(LookupError):
-    '''Username not found!'''
-class CommandError(LookupError):
-    '''Undefined command'''
-class DateError(LookupError):
-    '''Unsupported date format'''    
+            if name == 'all':
+                if self.keys():
+                    for sheet in self:
+                        return sheet
 
-
-def input_error(handler: tuple) -> str:
-    '''Return input error'''
-    errors = {
-        1: 'Enter valid command!',
-        2: 'Username not found',
-        3: 'Enter valid username and(or) phone (phone must contain only digits)',
-        4: 'Number not found',
-        5: 'Please enter date using format "YYYY-MM-DD"'
-    }
-    def trying(command: str, args) -> str:
+            else:
+                user = PrettyTable()
+                user.field_names = ['Name', 'Phone', 'Days to birthday']
+                user.add_row([
+                    self[name].name.value,
+                    self[name].phone.value,
+                    self[name].days_to_birthday()
+                    ])
+                return user
+            return 'End list'
         try:
-            answer = handler(command, args)
+            if command == 'hello':
+                return greet()
+            elif command == 'add':
+                return add(args[0], args[1], args[2])
+            elif command == 'change':
+                return change(args[0], args[1], args[2])
+            elif command == 'phone':
+                return show(args[0])
+            elif command == 'show':
+                return show(args[0])
+            raise CommandError
+        
         except UsernameError:
             return errors[2]
         except ValueError:
@@ -151,121 +149,85 @@ def input_error(handler: tuple) -> str:
             return errors[2]
         except DateError:
             return errors[5]
-        return answer
-    return trying
 
-@input_error
-def handler(command: str, args) -> str:
-    '''Handle commands'''
-    global ab
+    def parser(self, raw_input: str) -> str|tuple[str]:
+        '''Returns (command, name, number, birthday)'''
+        raw_input = str(raw_input)
+        if raw_input == 'good bye' or\
+        raw_input == 'close' or\
+        raw_input == 'exit' or\
+        raw_input == 'quit':
+            print('Good bye!')
+            return 'break'
 
-    def greet():
-        return 'Hello!\nHow can I help you?'
+        user_input = raw_input.split(' ')
+        if len(user_input) < 2:
+            return 'error'
+        command = user_input[0]
+        name = user_input[1]
+        number = None
+        birthday = None
+
+        if len(user_input) > 2:
+            for inp in user_input[2:]:
+                if inp.isnumeric():
+                    number = inp
+                    continue
+                birthday = inp
+
+        return (command, name, number, birthday)
+
+class Record:
+    def __init__(self, name, phone=None, birthday=None) -> None:
+        self.name = name
+        self.birthday = birthday
+        self.phone = phone
+
+    def days_to_birthday(self):
+        if self.birthday.value:
+            now = datetime.now()
+            birthday = self.birthday.value
+            next_birthday = birthday.replace(year=now.year)
+            if next_birthday < now:
+                next_birthday = next_birthday.replace(year=next_birthday.year + 1)
+            return next_birthday.toordinal() - now.toordinal()
+        return '-----'
 
 
-    def add(name: str, phone=None, birthday=None) -> str:
-        '''Add user number and/or birthday into database'''
-        try:
-            if birthday:
-                birthday = datetime.strptime(birthday, '%Y-%m-%d')
-        except ValueError as err:
-            raise DateError from err
+class Field:
+    def __init__(self) -> None:
+        self.value = {}
 
-        if name in ab.keys():
-            if phone:
-                phone = Phone(phone)
-                ab[name].add(phone)
-            if birthday:
-                birthday = Birthday(birthday)
-                ab[name].birthday = birthday
-            return 'Done'
+    def __setitem__(self, item, _):
+        self.value['value'] = item
 
-        name = Name(name)
-        if phone:
-            phone = Phone(phone)
-        birthday = Birthday(birthday)
-        rec = Record(name, phone, birthday)
-        ab.add_record(rec)
-        return 'Done'
+    def __getitem__(self, _):
+        return self.value['value']
 
 
-    def change(name, old_number, new_number) -> str:
-        '''Change user number'''
+@dataclass
+class Name(Field):
+    '''Name'''
 
-        if name not in ab.keys():
-            raise UsernameError
 
-        for phone in ab[name].phones:
-            if phone.value == old_number:
-                phone.value = new_number
-                return 'Done'
+@dataclass
+class Birthday(Field):
+    '''Birthday'''
 
-        raise ValueError
 
-    def show(name: str) -> str:
-        '''Show User phone.\n
-        If key: 'all' - show all contacts'''
+@dataclass
+class Phone(Field):
+    '''Phone'''
 
-        if name == 'all':
-            ab.show_all()
-            return ab_iter
 
-        days_to_birthday = '------'
-        if ab[name].birthday.value:
-            days_to_birthday = ab[name].days_to_birthday()
+class UsernameError(LookupError):
+    '''Username not found!'''
+class CommandError(LookupError):
+    '''Undefined command'''
+class DateError(LookupError):
+    '''Unsupported date format'''    
 
-        text = '|{:^15}|{:^15}|{:^16}|\n'.format(
-                                'Username',
-                                'Phone',
-                                'Days to birthday'
-                            )
-        for phone in ab[name].phones:
-            text += '|{:^15}|{:^15}|{:^16}|'.format(
-                                ab[name].name.value,
-                                phone.value,
-                                days_to_birthday
-                            )
-        return text
 
-    if command == 'hello':
-        return greet()
-    elif command == 'add':
-        return add(args[0], args[1], args[2])
-    elif command == 'change':
-        return change(args[0], args[1], args[2])
-    elif command == 'phone':
-        return show(args[0])
-    elif command == 'show':
-        return show(args[0])
-    raise CommandError
-
-def parser(raw_input: str) -> str|tuple[str]:
-    '''Returns (command, name, number, birthday)'''
-    raw_input = str(raw_input)
-    if raw_input == 'good bye' or\
-       raw_input == 'close' or\
-       raw_input == 'exit' or\
-       raw_input == 'quit':
-        print('Good bye!')
-        time.sleep(3)
-        return 'break'
-
-    user_input = raw_input.split(' ')
-    if len(user_input) < 2:
-        return 'error'
-    command = user_input[0]
-    name = user_input[1]
-    number = None
-    birthday = None
-
-    if len(user_input) > 2:
-        for inp in user_input[2:]:
-            if inp.isnumeric():
-                number = inp
-                continue
-            birthday = inp
-
-    return (command, name, number, birthday)
 
 def main() -> None:
     """Main cycle"""
@@ -282,10 +244,11 @@ def main() -> None:
                 'add nick 1992-1-5',
                 'show all',
                 'show nick',
-                'add nancy 159648 1552-4-7',
-                'add nana 5934 2002-4-7',
-                'add nency 1594648 1552-4-7',
-                'add nata 59314 2002-4-7',
+                'add nancy 159648 1552-4-5',
+                'add nana 5934 2002-4-17',
+                'add nency 1594648 1552-4-27',
+                'add nata 59314 2002-1-7',
+                'add naa 593153434 2021-1-8',
                 'show all',
                 'show all',
                 'show all',
@@ -299,12 +262,13 @@ def main() -> None:
                     ]
             print(test[index])
             command = test[index]
-        command = parser(command.lower())
+        command = ab.parser(command.lower())
         if command == 'break':
             break
-        result = handler(command[0], command[1:])
 
-        if isinstance(result, AddressBookIter):
+        result = ab.handler(command[0], command[1:])
+
+        if isinstance(result, AddressBook):
             for sheet in result:
                 print(sheet)
                 break
@@ -314,5 +278,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     ab = AddressBook()
-    ab_iter = AddressBookIter()
     main()
